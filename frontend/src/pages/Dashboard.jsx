b6d21bbbc92261296f003fa4faf0ae91178c55cd
manvigupta226 <manvigupta226@gmail.com>
2025-05-24 15:39:09 +0530

    bug fix 2

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import axios from "../api/axios";
 // import { useNavigate } from "react-router-dom";
 
 // const navigate = useNavigate();
--- frontend/src/pages/PDFDetailPage.jsx
+++ frontend/src/pages/PDFDetailPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios";
+import axios from "../api/axios";
 
 const PDFDetailPage = () => {
   const { id } = useParams();
